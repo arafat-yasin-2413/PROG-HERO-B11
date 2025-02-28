b60beef9c22141e59ab9bdf6f30cd7dfc06da204
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-03-01 01:07:34 +0600

    New things added from Module 28-1, 28-2

--- MILESTONE 5/payoo-mfs/script/addMoney.js
+++ MILESTONE 5/payoo-mfs/script/addMoney.js
@@ -1,6 +1,36 @@
+document.getElementById('cash-out-section').style.display = "none";
+
+
+document.getElementById('add-money-box').addEventListener('click', function(){
+    document.getElementById('add-money-section').style.display = 'block';
+    document.getElementById('cash-out-section').style.display = "none";
+})
+
+
+
+document.getElementById('cash-out-box').addEventListener('click', function(){
+    document.getElementById('cash-out-section').style.display = "block";    
+    document.getElementById('add-money-section').style.display = "none";    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
 
+    const accountNumber = document.getElementById('account-number').value;
 
     const amount = document.getElementById('amount').value;
     const convertedAmount = parseFloat(amount);
@@ -12,23 +42,39 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
 
 
 
-    if(amount && pinNumber) {
-        if(pinNumber === 1234) {
-            const sum = convertedMainBalance + convertedAmount;
+    // if(amount && pinNumber) {
+    //     if(pinNumber === 1234) {
+    //         const sum = convertedMainBalance + convertedAmount;
             
-            // document.getElementById('main-balance').innerText = sum;
+    //         // document.getElementById('main-balance').innerText = sum;
+    //         mainBalance.innerText = sum;
+    //         console.log('money add korsi');
+    
+    
+    //     }
+    //     else {
+    //         alert('pin sothik na');
+    //     }
+    // }
+
+    // else {
+    //     alert('enter amount');
+    // }
+
+    if(accountNumber.length === 11) {
+        if (pinNumber === 1234) {
+            const sum = convertedMainBalance + convertedAmount;
             mainBalance.innerText = sum;
-            console.log('money add korsi');
-    
-    
         }
+
         else {
-            alert('pin sothik na');
+            alert('pin number vul');
         }
+    
     }
 
     else {
-        alert('enter amount');
+        alert('account number thik nai');
     }
 
     
