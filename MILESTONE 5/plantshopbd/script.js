d2e2827d2e34e8489f8a842bf28952ba9fbf672d
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-03-04 14:47:10 +0600

    DOM and dynamic data showing practice Done

---
+++ MILESTONE 5/plantshopbd/script.js
@@ -0,0 +1,15 @@
+// document
+//   .getElementById("product-container")
+//   .addEventListener("click", function (e) {
+//     if (e.target.tagName == "BUTTON") {
+//       const button = e.target;
+//       // console.log(
+//       //   button.parentNode.parentNode.children[1].children[1].innerText
+//       // );
+//     }
+//   });
+
+
+document.getElementById('product-container').addEventListener('click', function(event){
+    console.log(event.target.parentNode.parentNode.children[1].children[1].innerText);
+})
