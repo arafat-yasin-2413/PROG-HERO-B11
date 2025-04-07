d4f0247825af61cf5a863a81481d5be64eb3dd9a
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-07 21:46:10 +0600

    all updated

---
+++ MILESTONE 7/awesome-water-bottles/src/components/Bottles/Bottles.jsx
@@ -0,0 +1,77 @@
+import React, { use, useEffect, useState } from 'react';
+import Bottle from '../Bottle/Bottle';
+import './Bottles.css'
+import { addToStoredCart, getStoreCart, removeFromCart } from '../../utilities/localstorage';
+import Cart from '../Cart/Cart';
+
+
+
+const Bottles = ({bottlesPromise}) => {
+
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart= (bottle)=> {
+        console.log('bottle will be added to the cart');
+        // console.log(bottle.name);
+
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+
+        addToStoredCart(bottle.id)
+    }
+
+
+    const handleRemoveFromCart = id => {
+        console.log('remove item from the cart ', id);
+
+        const remainingCart = cart.filter(bottle => bottle.id !== id);
+        setCart(remainingCart);
+
+        removeFromCart(id);
+    }
+
+    const bottles = use(bottlesPromise);
+    console.log(bottles);
+
+    // useEffect
+    useEffect( () => {
+        const storedCartIds = getStoreCart();
+        // console.log(storedCartIds, bottles);
+
+        const storedCart = [];
+
+        for (const id of storedCartIds) {
+            // console.log(id);
+            const cartBottle = bottles.find(bottle => bottle.id === id);
+
+            if (cartBottle){
+                storedCart.push(cartBottle);
+            }
+        }
+
+        console.log('stored cart , ', storedCart);
+        setCart(storedCart);
+
+
+    }, [bottles])
+
+    return (
+        <div>
+
+            <h3>Bottles: {bottles.length}</h3>
+            <p>Added to cart : {cart.length}</p>
+
+            <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+
+            <div className='bottles-container'>
+
+            {
+                bottles.map(bottle=> <Bottle key={bottle.id}
+                    handleAddToCart={handleAddToCart} bottle={bottle}></Bottle>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Bottles;
