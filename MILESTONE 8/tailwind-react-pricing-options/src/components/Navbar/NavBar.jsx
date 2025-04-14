a2f6763a15480f59090780bd77bfa26c679c8897
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-14 07:22:38 +0600

    44-3 Done

--- MILESTONE 8/tailwind-react-pricing-options/src/components/Navbar/NavBar.jsx
+++ MILESTONE 8/tailwind-react-pricing-options/src/components/Navbar/NavBar.jsx
@@ -1,57 +1,73 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const navigationData = [
-    {
-      id: 1,
-      path: "/home",
-      name: "Home"
-    },
-    {
-      id: 2,
-      path: "/about",
-      name: "About Us"
-    },
-    {
-      id: 3,
-      path: "/services",
-      name: "Services"
-    },
-    {
-      id: 4,
-      path: "/contact",
-      name: "Contact"
-    },
-    {
-      id: 5,
-      path: "/blog",
-      name: "Blog"
-    }
-  ];
-  
-
+	{
+		id: 1,
+		path: "/home",
+		name: "Home",
+	},
+	{
+		id: 2,
+		path: "/about",
+		name: "About Us",
+	},
+	{
+		id: 3,
+		path: "/services",
+		name: "Services",
+	},
+	{
+		id: 4,
+		path: "/contact",
+		name: "Contact",
+	},
+	{
+		id: 5,
+		path: "/blog",
+		name: "Blog",
+	},
+];
 
 const NavBar = () => {
-    return (
-        <nav>
 
-            <ul className="flex">
+    const [open, setOpen] = useState(false);
+    const links = navigationData.map((route) => (
+        <Link key={route.id} route={route}></Link>
+    ))
+
+	return (
+		<nav className="flex justify-between mx-10">
+			<span onClick={()=> setOpen(!open)} className="flex border gap-4">
+
                 {
-                    navigationData.map(route=> <Link key={route.id} route={route}></Link>)
+                    open ? 
+                    <X className="md:hidden"></X> : 
+                    <Menu className="md:hidden"></Menu>
                 }
-            </ul>
 
+                {/* small nav links */}
+                <ul className="md:hidden">
+                    {
+                        links
+                    }
+                </ul>
 
+				
+				<h3>My Navbar</h3>
+			</span>
 
+            {/* big nav links */}
+			<ul className="hidden md:flex">
+				{
+                    links
+                }
+			</ul>
 
+			<button>Sign In</button>
 
-
-
-
-
-
-
-            {/* 
+			{/* 
             <ul className="flex">
                 {
                     navigationData.map(route=> 
@@ -62,28 +78,13 @@ const NavBar = () => {
 
             </ul> */}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            {/* <ul className=" flex ">
+			{/* <ul className=" flex ">
                 <li><a className="mr-10" href="/">Home</a></li>
                 <li><a className="mr-10" href="/about">About</a></li>
                 <li><a className="mr-10" href="/blog">Blog</a></li>
             </ul> */}
-            
-        </nav>
-    );
+		</nav>
+	);
 };
 
 export default NavBar;
