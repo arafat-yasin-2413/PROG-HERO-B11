1e6b62fbd31564c7e1f9173cbd0bb76c9974d27f
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-18 10:54:37 +0600

    45-2 Done

--- MILESTONE 8/intro-to-react-router/src/main.jsx
+++ MILESTONE 8/intro-to-react-router/src/main.jsx
@@ -2,18 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router";
-
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './components/Home/Home.jsx'
+import Mobiles from './components/Mobiles/Mobiles.jsx'
+import Root from './components/Root/Root.jsx'
+import Laptops from './components/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hello from React Router</div>
+        Component: Root,
+        children: [
+            {index: true, Component: Home },
+            {path: 'mobiles', Component: Mobiles  },
+            {path: 'laptops', Component: Laptops},
+        ]
+    },
+
+    {
+        path: 'about',
+        element: <div>About me here</div>
+    },
+
+    {
+        path: 'blogs',
+        element: <div>All my blogs are here</div>
+    },
+
+    {
+        path: 'app',
+        Component: App
+    },
+
+    {
+        path: 'app2',
+        element: <App></App>
     }
 ])
 
