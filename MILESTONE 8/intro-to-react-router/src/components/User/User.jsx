dd5e9702712175405039c9feb7d3947c5119d66b
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-19 00:39:28 +0600

    Module 45 Done

--- MILESTONE 8/intro-to-react-router/src/components/User/User.jsx
+++ MILESTONE 8/intro-to-react-router/src/components/User/User.jsx
@@ -1,12 +1,16 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({user}) => {
 
     const [showInfo,setShowInfo] = useState(false);
-    const {id,name, email, phone } = user;
+    const [visitHome, setVisitHome] = useState(false); 
 
+    const location = useLocation();
+    console.log(location);
+
+    const {id,name, email, phone } = user;
 
 
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
@@ -19,6 +23,10 @@ const User = ({user}) => {
         padding: '10px',
         margin: '10px',
 
+    }
+
+    if(visitHome) {
+        return <Navigate to="/"></Navigate>
     }
 
     return (
@@ -42,6 +50,9 @@ const User = ({user}) => {
                     <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
             }
+
+
+            <button onClick={()=> setVisitHome(true)}>Visit Home</button>
              
         </div>
     );
