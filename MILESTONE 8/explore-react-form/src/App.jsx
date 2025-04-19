2a9f2c4226990e5d94a3cd3278c2bee23e1ab1a5
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-19 08:28:28 +0600

    46-3 Done

--- MILESTONE 8/explore-react-form/src/App.jsx
+++ MILESTONE 8/explore-react-form/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ControlledField from "./components/ControlledField/ControlledField";
 import FormAction from "./components/FormAction/FormAction";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
+import UncontrolledField from "./components/UncontrolledField/UncontrolledField";
 
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
 
             {/* <FormAction></FormAction> */}
 
-            <ControlledField></ControlledField>
+            {/* <ControlledField></ControlledField> */}
+
+            <UncontrolledField></UncontrolledField>
 		</>
 	);
 }
--- MILESTONE 8/explore-react-form/src/components/ControlledField/ControlledField.jsx
+++ MILESTONE 8/explore-react-form/src/components/ControlledField/ControlledField.jsx
@@ -1,35 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ControlledField = () => {
+	const [password, setPassword] = useState("secret");
+	const [error, setError] = useState("");
 
-    const [password, setPassword] = useState('secret')
-
-    const handleSubmit = (e)=> {
-        e.preventDefault();
-        console.log('submitted');
-    }
-
-    const handlePasswordOnChange = (e) => {
-        console.log(e.target.value);
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("submitted");
 
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" 
-                id="" placeholder='Email' required 
-                />
-                <br />
-                <input type="password" name="password" 
-                id="" placeholder='Password'  required
-                onChange={handlePasswordOnChange} defaultValue={password} 
-                />
-                <br />
-                <input type="submit" value="Submit" />
-            </form>      
-        </div>
-    );
+
+        if (password.length < 6) {
+			setError("Password must be 6 character or longer.");
+		}
+
+        else{
+            setError('');
+        }
+	};
+
+	const handlePasswordOnChange = (e) => {
+		console.log(e.target.value);
+		setPassword(e.target.value);
+
+		if (password.length < 6) {
+			setError("Password must be 6 character or longer.");
+		}
+
+        else{
+            setError('');
+        }
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="email"
+					name="email"
+					id=""
+					placeholder="Email"
+					required
+				/>
+				<br />
+				<input
+					type="password"
+					name="password"
+					id=""
+					placeholder="Password"
+					required
+					onChange={handlePasswordOnChange}
+					defaultValue={password}
+				/>
+				<br />
+				<input type="submit" value="Submit" />
+			</form>
+
+			<p style={{color: 'red'}}>
+                <small>
+                    {error}
+                </small>
+            </p>
+		</div>
+	);
 };
 
 export default ControlledField;
