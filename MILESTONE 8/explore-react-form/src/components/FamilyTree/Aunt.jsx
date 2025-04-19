df6c213859de839d6d64fa02dd465b0b1eca1f36
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-19 14:45:58 +0600

    46-8 Done

--- MILESTONE 8/explore-react-form/src/components/FamilyTree/Aunt.jsx
+++ MILESTONE 8/explore-react-form/src/components/FamilyTree/Aunt.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Cousin from './Cousin';
 
-const Aunt = () => {
+const Aunt = ({asset}) => {
     return (
         <div>
             <h3>Aunt</h3>
 
             <section className='flex'>
-                <Cousin name='Tom tom'></Cousin>
+                <Cousin  name='Tom Tom' asset={asset}></Cousin>
                 <Cousin name='Jo jooo'></Cousin>
             </section>
         </div>
--- MILESTONE 8/explore-react-form/src/components/FamilyTree/Special.jsx
+++ MILESTONE 8/explore-react-form/src/components/FamilyTree/Special.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
-const Special = ({name}) => {
+const Special = ({name, asset}) => {
+
+    const newAsset = useContext(AssetContext);
+    console.log('new asset', newAsset);
+
     return (
         <div>
             <h3>Special: {name}</h3>
+            <h4>Asset: {asset}</h4>
+            <h4>New Asset : {newAsset}</h4>
         </div>
     );
 };
