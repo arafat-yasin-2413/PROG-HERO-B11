627d766516148f6ca2018a03542ca3c1bed242ec
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-20 16:21:45 +0600

    Module 47 Done , Banner shown

--- MILESTONE 8/boi-poka/src/components/Banner/Banner.jsx
+++ MILESTONE 8/boi-poka/src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import bookImage from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-        <div className='flex justify-around p-16 items-center bg-slate-100 rounded-3xl my-6 shadow-md hidden'>
+        <div className='flex justify-around p-16 items-center bg-slate-100 rounded-3xl my-6 shadow-md'>
             <div className=''>
                 <h1 className='text-6xl mb-6 font-medium '>Books to freshen <br /> up your bookshelf</h1>
                 <button className='btn btn-primary'>View The List</button>
