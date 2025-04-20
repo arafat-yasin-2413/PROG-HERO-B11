0649e900c49aec77e8eff819493fd581f2a15683
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-04-20 06:34:15 +0600

    47-5 Done

--- MILESTONE 8/boi-poka/src/pages/Book/Book.jsx
+++ MILESTONE 8/boi-poka/src/pages/Book/Book.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({ book }) => {
 	console.log(book);
 
-    const { bookName, author, image  } = book;
+    const { bookName, author, image, rating, category  } = book;
 
 	return (
-		<div className="card bg-base-100 shadow-sm">
-			<figure className="p-3">
+		<div className="card bg-base-100 shadow-sm border border-amber-200 p-6">
+			<figure className="p-4 rounded-2xl bg-slate-100 w-2/3 mx-auto">
 				<img 
-                    className="w-28 "
+                    className="w-28 rounded-md"
 					src={image}
 					alt={`image of ${bookName}`}
 				/>
@@ -23,9 +24,9 @@ const Book = ({ book }) => {
 					A card component has a figure, a body part, and inside body
 					there are title and actions parts
 				</p>
-				<div className="card-actions justify-end">
-					<div className="badge badge-outline">Fashion</div>
-					<div className="badge badge-outline">Products</div>
+				<div className="card-actions justify-end ">
+					<div className="badge font-medium">{category}</div>
+					<div className="badge font-medium">{rating} <FaStarHalfAlt></FaStarHalfAlt> </div>
 				</div>
 			</div>
 		</div>
