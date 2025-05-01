f9d0977e3c8e14e093feced2349f79062e082e98
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-01 10:31:52 +0600

    52-2 Done

---
+++ MILESTONE 9/dragon-news/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-5xl'>hello</h2>
+
+      <button className="btn btn-primary">click</button>
+    </>
+  )
+}
+
+export default App
