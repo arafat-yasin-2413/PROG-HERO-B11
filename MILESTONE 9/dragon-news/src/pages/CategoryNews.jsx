5187d9aca72d0da49c892bef2c0e6f9fbcdc54cf
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-02 10:21:18 +0600

    52-8 Done

--- MILESTONE 9/dragon-news/src/pages/CategoryNews.jsx
+++ MILESTONE 9/dragon-news/src/pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import NewsCard from '../components/NewsCard';
 
 const CategoryNews = () => {
 
@@ -39,10 +40,20 @@ const CategoryNews = () => {
     },[data, id])
 
 
+    console.log(categoryNews);
 
     return (
         <div>
-            Total {categoryNews.length} news found
+            <h2 className='font-bold mb-5'>
+                Total <span className='text-secondary'>{categoryNews.length}</span> news found
+            </h2>
+
+
+            <div className='grid grid-cols-1 gap-5'>
+                {
+                    categoryNews.map((news)=> <NewsCard key={news.id} news ={news} ></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
