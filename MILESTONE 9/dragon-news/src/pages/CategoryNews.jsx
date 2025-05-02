4a76233f6a8d9f2ec565d06ac8aaa7b01ff71e65
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-02 08:44:47 +0600

    52-7 Done

--- MILESTONE 9/dragon-news/src/pages/CategoryNews.jsx
+++ MILESTONE 9/dragon-news/src/pages/CategoryNews.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
 
+    const [categoryNews , setCategoryNews] = useState([]);
+
+
     const {id} = useParams();
-    console.log(id);
+    // console.log(id);
+
+    const data = useLoaderData();
+
+    // console.log(data);
+
+
+    useEffect(()=>{
+
+        if (parseInt(id) === 0) {
+            setCategoryNews(data)
+            return;
+        }
+
+        else if(parseInt(id) === 1){
+            const filteredNews = data.filter((news)=> news.others.is_today_pick === true) 
+            setCategoryNews(filteredNews)
+            
+        }
+
+        else{
+
+            const filteredNews = data.filter((news)=> news.category_id === parseInt(id));
+            // console.log(filteredNews);   
+            setCategoryNews(filteredNews);
+        }
+
+
+
+    },[data, id])
+
 
 
     return (
         <div>
-            Category News id = {id}
+            Total {categoryNews.length} news found
         </div>
     );
 };
